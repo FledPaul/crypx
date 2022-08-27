6e730d189cf042471abd9959903f6fa9605e6ba8
Paul <fled@tuta.io>
2022-08-27 02:56:16 +0200

    The Splash Screen is now fetched from a server.
    The logo is now requested from a server, local files are no longer needed.

--- build/splashScreen.py
+++ build/splashScreen.py
@@ -1,10 +1,10 @@
 # Import sys
-import sys
+import sys, requests
 
 # Import PyQt5 stuff
 from PyQt5 import QtCore
 from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
-from PyQt5.QtGui import QPixmap
+from PyQt5.QtGui import QPixmap, QImage
 from PyQt5.QtCore import QTimer
 
 
@@ -14,17 +14,21 @@ class SplashScreen(QMainWindow):
         super().__init__()
             
         # Sets the window flags for frameless and top hints.
-        flags = QtCore.Qt.WindowFlags(QtCore.Qt.FramelessWindowHint | QtCore.Qt.WindowStaysOnTopHint)
+        flags = QtCore.Qt.WindowFlags(QtCore.Qt.FramelessWindowHint | QtCore.Qt.WindowStaysOnTopHint)         
 
         # Setup the logo and pixmap
         self.setFixedSize(650, 400)
         self.setWindowFlags(flags)
         self.setStyleSheet('background-color: #FFFFFF;')
-        self.pixmap = QPixmap('img/logo.png')
+         
+        # Get image from server
+        urlImage = 'https://file.fled.dev/crypx/latest.png'
+        Image = QImage()
+        Image.loadFromData(requests.get(urlImage).content)
             
         # Create QLabel and set pixmap.
         self.label = QLabel(self)
-        self.label.setPixmap(self.pixmap)
+        self.label.setPixmap(QPixmap(Image))
         self.label.move(0, 23)
         self.label.resize(650, 354)
 
@@ -33,7 +37,6 @@ class SplashScreen(QMainWindow):
 app = QApplication(sys.argv)
 splashScreen = SplashScreen()
 splashScreen.show()
-
 
 # Quit app and exit (after 2000 milliseconds)
 QTimer.singleShot(2000, splashScreen.close)
